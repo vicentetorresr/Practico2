b21e5cfc9fb884398771fe9c6b462b501accf4ac
Angelo <Alumno@TALLER_PC5>
2024-09-25 09:42:56 -0300

    Creacion de models Tarea

---
+++ Practico2/Models/Tarea.cs
@@ -0,0 +1,18 @@
+﻿namespace Practico2.Models
+{
+    public class Tarea
+    {
+      public int Id { get; set; }
+      public DateTime FechaInicio {get; set;}
+      public string EstadoTarea { get; set;}
+      public int Horas { get; set;}
+
+      public string Area { get; set;}
+      public int IdProyecto { get; set;}
+      public int IdEmpleado { get; set;}
+
+
+
+
+    }
+}
