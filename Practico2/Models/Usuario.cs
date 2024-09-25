35befb76efe5f3a202de3d7b100ed2e7efaff057
Angelo <Alumno@TALLER_PC5>
2024-09-25 09:47:48 -0300

    Implementacion de Usuario y errores

--- Practico2/Models/Usuario.cs
+++ Practico2/Models/Usuario.cs
@@ -1,4 +1,5 @@
 using Practico2.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Practico2.Models
 {
@@ -6,37 +7,14 @@ namespace Practico2.Models
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Apellido { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int RolId { get; set; }
-        public Roles Rol { get; set; } 
 
-        // Simulación de la carga de un usuario de prueba
-        public static Usuario CargarUsuario()
-        {
-            return new Usuario
-            {
-                Id = 1,
-                Nombre = "Angelo Maureira",
-                Email = "Angelo@example.com",
-                Password = "123456",
-                RolId = 1 // Se referencia el rol aca con el rolid
-            };
-        }
-        public static List<Usuario> CargarUsuarios()
-        {
-            return new List<Usuario>
-            {
-                new Usuario
-                {
-                    Id = 1,
-                    Nombre = "Angelo Maureira",
-                    Email = "Angelo@example.com",
-                    Password = "123456",
-                    RolId = 1 // Administrador
-                }               
-            };
-        }
+     
+
+        [ForeignKey("Rol")]
+        public int RolId { get; set; }
     }
 }
 
